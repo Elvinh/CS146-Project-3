cc3fea9ea61c73aec05c42544319b7db4e36bf80
sanshoustyle <thienvan@berkeley.edu>
2015-09-24 18:20:05 -0700

    Thien commit#1
    testing by adding Hello World.

---
+++ src/hashtable/HelloWorld.java
@@ -0,0 +1,11 @@
+package hashtable;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello, this is Thien. Making a commit");
+	}
+
+}
