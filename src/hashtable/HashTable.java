f0e744ac7505e787a32c4fab03263a1816b05255
Elton Vinh <eltonv93@gmail.com>
2015-09-24 18:02:59 -0700

    created hashtable interface
    created function declarations

---
+++ src/hashtable/HashTable.java
@@ -0,0 +1,25 @@
+package hashtable;
+
+import java.util.LinkedList;
+
+public class HashTable<K,V> {
+	private static final int MAX_SIZE = 100;
+
+	LinkedList<Cell<K,V>>[] table;
+	
+	int count;
+	
+	HashTable() {
+	}
+	void put(V value) {
+		
+	}
+	V get(K key) {
+		
+		return null;
+	}
+	int hashingFunction(K key) {
+		int index = -1;
+		return index;
+	}
+}
